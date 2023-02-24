a6641015f1ec5c66bbbef237e7febcbc18a5b174
DouZ <doucx2020@163.com>
2023-02-24 13:27:26 +0800

    二次提交

--- LoginSimulation.py
+++ LoginSimulation.py
@@ -13,10 +13,10 @@ browser.get('https://login.taobao.com')
 browser.implicitly_wait(10)
 # 获取页面元素模拟用户输入和点击行为
 username_input = browser.find_element_by_xpath('//*[@id="fm-login-id"]')
-username_input.send_keys('***********')
-time.sleep(2)
+username_input.send_keys('*********')  # 输入账号
+time.sleep(2) 
 password_input = browser.find_element_by_xpath('//*[@id="fm-login-password"]')
-password_input.send_keys('***********')
+password_input.send_keys('***********') # 输入密码
 login_button = browser.find_element_by_xpath('//*[@id="login-form"]/div[4]/button')
 login_button.click()
 # 显示等待
